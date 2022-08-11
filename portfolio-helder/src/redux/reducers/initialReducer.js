4a173559bf5fdbc920f2c036f43daee48e3fe6ed
helderdsa <helder_dsa@hotmail.com>
2022-08-11 18:42:23 -0300

    config inicial redux

---
+++ portfolio-helder/src/redux/reducers/initialReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  nome: 'Helder Daniel Silva Araujo' 
+};
+
+const initialReducer = (state = INITIAL_STATE, action) => {
+ switch(action.type) {
+   default:
+    return state;
+ }
+}
+
+export default initialReducer;
