5325f05b13aba9bf63b5802c59834d6ebb60f6c5
Helder Daniel <59895101+helderdsa@users.noreply.github.com>
2023-05-15 20:37:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 function App() {
   useEffect(() => {
-    document.title = 'Helder Daniel ';
+    document.title = 'Helder Daniel';
   }, [])
 
   return (
