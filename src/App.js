3c69fff12e67194f85c910122507a3aa5d9f4188
Helder Daniel <59895101+helderdsa@users.noreply.github.com>
2023-05-15 20:35:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 function App() {
+  useEffect(() => {
+    document.title = 'Helder Daniel ';
+  }, [])
+
   return (
     <Routes>
       <Route exact path="/" element={ <Home/> } />
